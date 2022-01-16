f28a459a5c33288e10f66e7add4dd02b85d39139
andy <yijianan1426@gmail.com>
2022-01-16 23:09:09 +0800

    add front-end code

--- 03_polkadot-js-api/kitties/frontend/src/Kitties.js
+++ 03_polkadot-js-api/kitties/frontend/src/Kitties.js
@@ -1,24 +1,55 @@
 import React, { useEffect, useState } from 'react'
-import { Form, Grid } from 'semantic-ui-react'
+import { Form, Grid, Item } from 'semantic-ui-react'
 
 import { useSubstrate } from './substrate-lib'
 import { TxButton } from './substrate-lib/components'
 
 import KittyCards from './KittyCards'
 
+const constructKitty = (hash, { dna, owner }) => ({
+  id: hash,
+  dna,
+  owner: owner.toJSON()
+});
+
 export default function Kitties (props) {
   const { api, keyring } = useSubstrate()
   const { accountPair } = props
 
+  const [kittiesCount, setKittiesCount] = useState([])
   const [kitties, setKitties] = useState([])
   const [status, setStatus] = useState('')
 
-  const fetchKitties = () => {
+  const fetchKittiesCount = () => {
     // TODO: 在这里调用 `api.query.kittiesModule.*` 函数去取得猫咪的信息。
     // 你需要取得：
     //   - 共有多少只猫咪
     //   - 每只猫咪的主人是谁
     //   - 每只猫咪的 DNA 是什么，用来组合出它的形态
+    // let unsub = null;
+
+    // const asyncFetch = async () => {
+    //   unsub = await api.query.KittiesModule.kitties.multi(kittyHashes, kitties => {
+    //     const kittyArr = kitties
+    //       .map((kitty, ind) => constructKitty(kittyHashes[ind], kitty.value));
+    //     setKitties(kittyArr);
+    //   });
+    // };
+
+    // asyncFetch();
+
+    // // return the unsubscription cleanup function
+    // return () => {
+    //   unsub && unsub();
+    // };
+
+    api.query.kittiesModule.kittiesCount(
+      c => {
+        if(c > 0) {
+          setKittiesCount(_.parseInt(c))
+        }
+      }
+    ).catch(console.error)
   }
 
   const populateKitties = () => {
@@ -31,12 +62,28 @@ export default function Kitties (props) {
     //  }, { id: ..., dna: ..., owner: ... }]
     //  ```
     // 这个 kitties 会传入 <KittyCards/> 然后对每只猫咪进行处理
-    const kitties = []
-    setKitties(kitties)
+    // const kitties = []
+    // setKitties(kitties)
+
+    if (kittiesCount > 0) {
+      const idArr = [...new Array(kittiesCount).keys()]
+      const ownerQuery = api.query.kittiesModule.owner.multi(idArr)
+      const kittiesQuery = api.query.kittiesModule.kitties.multi(idArr)
+      Promise.all([ownerQuery, kittiesQuery]).then( r => {
+        const [_owner, _kitties] = r
+        setKitties(_.map(_kitties, (item, i) => {
+          return {
+            id: i,
+            dna: item.unwrap(),
+            owner: _owner[i].unwrap().toString() 
+          }
+        }))
+      })
+    }
   }
 
-  useEffect(fetchKitties, [api, keyring])
-  useEffect(populateKitties, [])
+  useEffect(fetchKittiesCount, [api, keyring])
+  useEffect(populateKitties, [api, kittiesCount])
 
   return <Grid.Column width={16}>
     <h1>小毛孩</h1>
@@ -46,7 +93,7 @@ export default function Kitties (props) {
         <TxButton
           accountPair={accountPair} label='创建小毛孩' type='SIGNED-TX' setStatus={setStatus}
           attrs={{
-            palletRpc: 'kittiesModule',
+            palletRpc: 'KittiesModule',
             callable: 'create',
             inputParams: [],
             paramFields: []
